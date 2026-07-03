dd7c01b3e95b2f582b0d7d0dcadd66d19609ec01
RadaPraesiri <69070170@kmitl.ac.th>
2026-07-03 12:12:39 +0700

    test

---
+++ Python/Ex/name.py
@@ -0,0 +1,9 @@
+"""name"""
+def main():
+    '''name'''
+    #code
+    name = str(input())
+    sname = str(input())
+    print(f"Hello {name} {sname}")
+    print(name[:2]+sname[:2])
+main()
---
+++ Python/Homework/สลับตัวอักษร/hw1.py
@@ -0,0 +1,7 @@
+"""name"""
+def main():
+    '''name'''
+    #code
+    name = str(input())
+    print(f"{name.lower()[::-1]}")
+main()
