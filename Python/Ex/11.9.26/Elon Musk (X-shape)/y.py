a6e673d18096cd4b72267455e0bf196c2928bc50
ซาตา <69070203@kmitl.ac.th>
2026-09-11 00:45:58 +0700

    a

--- Python/Ex/11.9.26/Elon Musk (X-shape)/y.py
+++ Python/Ex/11.9.26/Elon Musk (X-shape)/y.py
@@ -1,6 +1,17 @@
-y = 64
-a = "a"
-x = ord(a)
-z = chr(y)
-print(x)
-print(z)
+x,y = input().split()
+x=int(x)
+mid = x//2
+for i in range(x):
+    for j in range(x):
+        if y == "#":
+            if i == j or j == x - 1 - i:
+                print("#",end="")
+            else:
+                print("-",end="")
+        else:
+            if i == j or j == x - 1 - i:
+                let = ord(y)
+                print(chr(let+abs(i-mid)),end="")
+            else:
+                print("-",end="")
+    print()
--- Python/Ex/25.9.26/ใส่กล่อง/x.py
+++ Python/Ex/25.9.26/ใส่กล่อง/x.py
@@ -1,9 +1,7 @@
+"""ใส่กล่อง"""
 W, L, M, N = map(int, input().split())
-
 ans = float('inf')
-
 for A in range(M, N + 1):
     waste = (W % A) * (L % A)
     ans = min(ans, waste)
-
 print(ans)
